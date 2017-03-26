1a6c556d0de9b9483951d07512d595052bce5968
Rubmary Rojas <rubmaryrojas@gmail.com>
2017-03-26 01:39:40 -0400

    Terminadas funciones de dibujo

--- Funciones para dibujar/dibujo.hs
+++ Funciones para dibujar/dibujo.hs
@@ -1,22 +1,23 @@
 module Main(main) where
 import System.IO
-import Data.Set as Set
-
 
 ----------------------------------------- Tipos de Datos------------------------------
-data Point = Point  { x ::  Int
-                    , y ::  Int }
+data Point = Point  { x ::  Double        -- Coordenada x
+                    , y ::  Double }      -- Coordenada y
 
-data Arc  = Arc {   pos'  :: Point
-                  , r     :: Double
-                  , alpha :: Double
-                  , dir'  :: Double }
+data Arc  = Arc {   pos   :: Point        -- Posicion inicial, centro del arco    
+                  , r     :: Double       -- Radio del arco
+                  , alpha :: Double       -- Angulo de giro
+                  , dir   :: Double }     -- Direccion inicial del arco
 
-data Seg = Seg {  pos   :: Point
-                , l     :: Double
-                , dir   :: Double }
+data Seg = Seg {  pos'  :: Point          -- Posicion inicial, punto inicial del segmento
+                , l     :: Double         -- Longitud del segmento
+                , dir'  :: Double }       -- Direccion: angulo del segmento respecto al eje x
 
 
+-- Constructor de Point usando un par de enteros
+fromPair :: (Int, Int) -> Point
+fromPair (x, y) = Point (fromIntegral x) (fromIntegral y)
 
 -- Definicion de operadores entre puntos
 instance Num Point where
@@ -24,165 +25,105 @@ instance Num Point where
   (Point x1 y1) - (Point x2 y2) = Point (x1-x2) (y1-y2)       -- Resta de dos Puntos
   (Point x1 y1) * (Point x2 y2) = Point (x1*x2) (y1*y2)
   abs     (Point x y)  = Point (abs x) (abs y)
-  signum  (Point x y)  = Point (x `div` (abs x)) (y `div` (abs y))
+  signum  (Point x y)  = Point (x / (abs x)) (y / (abs y))
   fromInteger n        = Point (fromInteger n) (fromInteger n)
 
 -- Producto punto entre puntos
-(^*) :: Point -> Point -> Int
+(^*) :: Point -> Point -> Double
 (Point x1 y1) ^* (Point x2 y2) = x1*x2 + y1*y2
 
 -- Producto cruz entre puntos
-(**) :: Point -> Point -> Int
+(**) :: Point -> Point -> Double
 (Point x1 y1) ** (Point x2 y2) = x1*y2 - y2*x1
+
+-- Multiplicacion de un punto por un escalar
+(*/) :: Point -> Double -> Point
+(Point x y) */ a = Point (x*a) (y*a)
+
+-- Distancia al cuadrado entre dos puntos
+dist2 :: Point -> Point -> Double
+dist2 p1 p2 = (p1 - p2)^*(p1 - p2)
 
 -- Distancia entre dos puntos
 dist :: Point -> Point -> Double
-dist p1 p2 = sqrt $ fromIntegral $ (p1 - p2)^*(p1 - p2)
-
--- Distancia al cuadrado entre dos puntos
-dist2 :: Point -> Point -> Int
-dist2 p1 p2 = (p1 - p2)^*(p1 - p2)
-
+dist p1 p2 = sqrt $  dist2 p1 p2
 
 -- Angulo de un vector respecto al eje x positivo
 ang :: Point -> Double
-ang (Point x y) = (positive $ atan2 y' x')*180/pi
-  where x' = fromIntegral x
-        y' = fromIntegral y
-        positive a = if (a < 0) then (2*pi)+a else a 
-
-
----- Puntos con coordenadas reales
-
-data PointD = PointD  { x' ::  Double
-                      , y' ::  Double }
-
--- Multiplicacion de un punto con coordenadas reales por un escalar
-(*/) :: PointD -> Double -> PointD
-(PointD x y) */ a = PointD (x*a) (y*a)
-
--- Suma de Puntos con coordenadas reales
-(+/) :: PointD -> PointD -> PointD
-(PointD x1 y1) +/ (PointD x2 y2) = PointD (x1+x2) (y1+y2)
-
--- Resta de Puntos con coordenadas reales
-(-/) :: PointD -> PointD -> PointD
-(PointD x1 y1) -/ (PointD x2 y2) = PointD (x1-x2) (y1-y2)
-
--- Producto punto con coordenadas reales
-(^/) :: PointD -> PointD -> Double
-(PointD x1 y1) ^/ (PointD x2 y2) = x1*x2 + y1*y2
-
-
--- De un punto con coordenadas enteras a uno con coordenadas reales
-fromPoint :: Point -> PointD
-fromPoint (Point x y) = PointD (fromIntegral x) (fromIntegral y)
-
--- norma
-norma :: PointD -> Double
-norma (PointD x y) = sqrt $ x*x + y*y
+ang (Point x y) = (positive $ atan2 y x)*180/pi
+  where positive a = if (a < 0) then (2*pi)+a else a 
 
 
 -- Proyeccion del punto c en el segmento ab
-proPointSeg  ::  Point -> PointD -> Point -> PointD
-proPointSeg a b' c  
-    | d*d < 1         = a'
-    | r   < 0         = a'
-    | r   > 1         = b'
-    | otherwise     = a' +/ ((b' -/ a') */ r)
-    where d   = norma (a' -/ b')
-          a'  = fromPoint a
-          c'  = fromPoint c
-          r  = ((c' -/ a') ^/ (b'-/ a')) / (d*d)
+proPointSeg  ::  Point -> Point -> Point -> Point
+proPointSeg a b c  
+    | d*d < 1         = a
+    | r   < 0         = a
+    | r   > 1         = b
+    | otherwise     = a + ((b-a) */ r)
+    where d   = dist2 a b
+          r  = ((c - a)^*(b-a)) / d
 
 
 -- Distancia entre el segmento ab y el punto c
 distPointSeg  ::  Point -> Point -> Point -> Double
-distPointSeg a b c = norma ((fromPoint c) -/ proPointSeg a (fromPoint b) c)
+distPointSeg a b c = dist c (proPointSeg a b c)
 
 ---------------------- Verificar si pertenece al objeto deseado -------------------
+
 -- Verifica si un punto esta en el arco descrito
 isInArc   :: Point -> Arc -> Bool
 isInArc pt (Arc pc r al dir) = 
-  if (abs((dist pt pc)-r) <= (sqrt 2)*0.5) && (validAng (pt-pc) inicio fin)  then True else False
+  if (abs((dist pt pc)-r) < eps && (validAng (pt-pc) inicio fin)) || (abs(dist pt pc) < 2*eps) then True else False
       where (inicio, fin) = (dir, dir-al)
+            eps = (sqrt 1.9)*0.5
             validAng p a b =  (a <= ang p && ang p <= b) ||
-                              (b <= ang p && ang p <= a) || 
-                              (a <= (ang p) + 360 && (ang p) + 360 <= b) ||
-                              (b <= (ang p) - 360 && (ang p) - 360 <= a)
+                              (b <= ang p && ang p <= a) ||
+                              (a + 360 <= (ang p) && (ang p) <= b + 360) ||
+                              (b + 360 <= (ang p) && (ang p) <= a + 360) ||
+                              (b + 720 <= (ang p) && (ang p) <= a + 720)
                               
-                              
+            
 -- Verifica si el punto esta en el segmento descrito
-isInSeg :: Seg -> Point -> Bool
-isInSeg (Seg pos l dir) p = (distPointSeg pos b p) < (sqrt 2)*0.5
+isInSeg :: Point -> Seg -> Bool
+isInSeg p (Seg pos l dir) = (distPointSeg pos b p) < (sqrt 1.9)*0.5
     where 
           al = dir*pi/180
-          b  = pos + (Point (floor $ l * (cos al)) (floor $ l * (sin al)))
+          b  = pos + (Point (l * (cos al)) (l * (sin al)))
           
-             
---------------------- Dibujar, funciones temporales ---------------------------------
--- Dibujar el arco
-drawArc :: Arc -> [[String]]
-drawArc  arc =
-    reverse [[if  (isInArc (Point x y) arc) then "1 " else "0 " | x <- [0..floor (2*r)] ] | y <- [0..floor (2*r)]]
-        where (Arc pt r al dir) = arc
 
 
--- Dibujar el segmento
-drawSeg :: Seg -> [[String]]
-drawSeg  seg = reverse [[if  (isInSeg seg (Point x y)) then "1 " else "0 " | x <- [0..1000] ] | y <- [0..1000]]
+---------------------- Funciones y estructuras para manejar ambos datos ---------------------------
 
+-- Trazos de retina, o es un arco o es un segmento
+type Line   = Either Arc Seg
+
+-- Determina si un punto está en un trazo (arco o segmento)
+isInLine :: (Int, Int) -> Line -> Bool
+isInLine p' line = let p = fromPair p' in either (isInArc p) (isInSeg p) line
+
+-- Determina si un punto pertence a al menos uno de los trazos dibujados
+isOn :: (Int, Int) -> [Line] -> Bool
+isOn p lines = elem True [isInLine p line | line <- lines]
+
+-- dibuja todos los trazos simultaneamente
+draw :: [Line] -> [String]
+draw lines = reverse [[if (isOn (x, y) lines) then '1' else '0' | x<-[-500..500] ] | y<-[-500..500] ]
 
 
 ---------------------------------------- Crear archivo, temporal ----------------------------
--- Imprimir una linea
-printLine :: [String] -> IO()
-printLine line = do mapM_ putStr line
-                    putStrLn ""
 
+-- Trazos a testear
+input :: [Line]
+input = [ Left  (Arc (Point 100     (-100)) 200 60  (-100))
+        , Right (Seg (Point 200     200)    400     (-120))
+        , Left  (Arc (Point (-50)   30)     150 360 (-60))
+        , Right (Seg (Point (-100)  (-300)) 500     90)
+        , Left  (Arc (Point (-500)   (-500))   350 (-90) 0)
+        ]
 
-readMaybe :: (Read a) => String -> Maybe a
-readMaybe st = case reads st of [(x, "")] -> Just x
-                                _ -> Nothing
-                    
-
-
--- Cambiar main' por main para probar circunferencias
-main' :: IO ()
-main' = do 
-      --    putStrLn "Introduzca el radio:"
-          line1 <- getLine
-      --    putStrLn "Introduzca el angulo (grados):"
-          line2 <- getLine
-      --    putStrLn "Introduzca la direccion:"
-          line3 <- getLine
-          let num       = readMaybe line1 :: Maybe Double
-          let alpha     = readMaybe line2 :: Maybe Double
-          let direccion = readMaybe line3 :: Maybe Double
-          let Just r    = num
-          let Just al   = alpha
-          let Just dir  = direccion
-          let c = Point (floor r) (floor r)
-          putStrLn "P1"
-          putStrLn $ show (2*(x c)+1) ++ " " ++ show (2*(y c)+1)
-          mapM_ printLine $ drawArc (Arc c r al dir)
-          
-
-
--- Cambiar main' por main para probar lineas
-main :: IO ()
-main   = do
-        -- Obtener posicion
-        line1 <- getLine    -- obtener x
-        line2 <- getLine    -- obtener y
-        line3 <- getLine    -- obtener direccion
-        line4 <- getLine    -- obtener longitud
-        let (Just x, Just y) =  (readMaybe line1 :: Maybe Int, readMaybe line2:: Maybe Int)
-        let Just dir        =  readMaybe line3 :: Maybe Double
-        let Just l          =  readMaybe line4 :: Maybe Double
-        putStrLn "P1\n1001 1001\n"
-        mapM_ printLine $ drawSeg (Seg (Point x y) l dir)
-
+main = do putStrLn "P1\n1001\n1001\n"
+          mapM_ putStrLn $ draw input
 
 
 
