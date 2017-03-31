0e86cb05adf242d9f4b16ec3933950ab5329d998
Rubmary Rojas <rubmaryrojas@gmail.com>
2017-03-31 19:31:20 -0400

    Integrado modulo de dibujo

--- retina.hs
+++ retina.hs
@@ -21,6 +21,7 @@ import Control.DeepSeq
 import Control.Exception
 import RunMonad as Run
 import RunFunciones as RF
+import DrawingMonad
 
 -- Obtener archivo con el formato correcto, si no es el archivo correcto
 -- se obtiene un error
@@ -30,12 +31,17 @@ filePath (x:y:_) = error "Introduzca un solo argumento."
 filePath (x:_) = case P.reverse x of  ('n':'t':'r':'.':_) -> x
                                       (y:_) -> error "Formato de archivo incorrecto."
 
+
+-- Obtiene el nombre del archivo sin la extension .rtn
+fileName :: String -> String
+fileName s = P.reverse $ trunk $ P.reverse $ s
+    where trunk (x:y:z:w:rname) = rname
+
 -- Funcion principal
 -- Abre el archivo, procesa los tokens, si hay alguno incorrecto, muestra todos los
 -- tokens incorrectos con su posicion. Si todos los tokens son correctos, analiza la
 -- sintaxis, si es correcta imprime el arbol, en caso contrario se obtiene un mensaje
 -- de error
-
 
 main::IO ()
 main = do
@@ -52,5 +58,6 @@ main = do
                 let context = evalState (Fun.start parse) RM.initialState
                 fs <- evaluate $ force context
                 (s, w) <- execRWST (RF.start parse) "" Run.initialState
+                image (fileName $ head $ args) (F.toList w)                                             -- Crear imagen
                 putStr $ ""
       
